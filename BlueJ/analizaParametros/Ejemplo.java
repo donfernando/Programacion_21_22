d0aafa51d812a02e3d6591c9cfc32f673ceca889
donfernando <fernandotoboso@gmail.com>
2022-03-18 11:08:32 +0000

    actualizado_el_2022_03_18

---
+++ BlueJ/analizaParametros/Ejemplo.java
@@ -0,0 +1,22 @@
+
+import java.awt.Point;
+
+public class Ejemplo
+{
+    public static void modifica(StringBuffer sb) {
+        sb.append("****");
+        System.out.println(sb);
+    }
+    public static void modifica(String s) {
+        s=s+"xxxx";
+       System.out.println(s);
+    }
+    public static void modifica(Point p1) {
+        p1.x--;
+    }
+    public static void modifica(int n) {
+        n++;
+        System.out.println(n);
+    }
+
+}
--- BlueJ/analizaParametros/Examen.java
+++
@@ -1,9 +0,0 @@
-
-import java.awt.Point;
-
-public class Examen
-{
-    public static void mueveParaArriba(Point p1) {
-        p1.x--;
-    }
-}
